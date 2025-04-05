9fe90eb45179861948cfb6b95e817bdadfb67883
Wesley Weisenberger <wesleynw@pm.me>
2025-04-05 14:07:55 -0400

    fix changed line

--- api/internal/db/schema.sql
+++ api/internal/db/schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE follows (
 );
 
 CREATE TABLE likes (
-    post_id INT,
+    post_id INT NOT NULL,
     comment_id INT,
     user_id INT NOT NULL,
     is_post BOOLEAN NOT NULL DEFAULT true,
