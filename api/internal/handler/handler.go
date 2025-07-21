d761d017f6ffb5d980e8064ba6ace0c7792d3431
Wesley Weisenberger <wesleynw@pm.me>
2025-07-21 18:28:33 -0400

    add otel

--- api/internal/handler/handler.go
+++ api/internal/handler/handler.go
@@ -6,6 +6,7 @@ import (
 	"splajompy.com/api/v2/internal/db/queries"
 	"strconv"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"splajompy.com/api/v2/internal/service"
 )
 
@@ -35,63 +36,71 @@ func NewHandler(queries queries.Querier,
 }
 
 func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
+	// handleFunc is a replacement for mux.HandleFunc
+	// which enriches the handler's HTTP instrumentation with the pattern as the http.route.
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		// Configure the "http.route" for the HTTP instrumentation.
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
 	// auth
-	mux.HandleFunc("POST /register", h.Register)
-	mux.HandleFunc("POST /login", h.Login)
-	mux.HandleFunc("POST /otc/generate", h.GenerateOTC)
-	mux.HandleFunc("POST /otc/verify", h.VerifyOTC)
-	mux.HandleFunc("POST /account/delete", h.DeleteAccount)
+	handleFunc("POST /register", h.Register)
+	handleFunc("POST /login", h.Login)
+	handleFunc("POST /otc/generate", h.GenerateOTC)
+	handleFunc("POST /otc/verify", h.VerifyOTC)
+	handleFunc("POST /account/delete", h.DeleteAccount)
 
 	// posts
-	mux.HandleFunc("GET /post/presignedUrl", h.GetPresignedUrl)
-	mux.HandleFunc("POST /post/new", h.CreateNewPost)
-	mux.HandleFunc("POST /v2/post/new", h.CreateNewPostV2)
-	mux.HandleFunc("GET /post/{id}", h.GetPostById)
-	mux.HandleFunc("GET /user/{id}/posts", h.GetPostsByUserId)
-	mux.HandleFunc("DELETE /post/{id}", h.DeletePostById)
-	mux.HandleFunc("POST /post/{id}/report", h.ReportPost)
+	handleFunc("GET /post/presignedUrl", h.GetPresignedUrl)
+	handleFunc("POST /post/new", h.CreateNewPost)
+	handleFunc("POST /v2/post/new", h.CreateNewPostV2)
+	handleFunc("GET /post/{id}", h.GetPostById)
+	handleFunc("GET /user/{id}/posts", h.GetPostsByUserId)
+	handleFunc("DELETE /post/{id}", h.DeletePostById)
+	handleFunc("POST /post/{id}/report", h.ReportPost)
 
 	// polls
-	mux.HandleFunc("POST /post/{post_id}/vote/{option_index}", h.VoteOnPost)
+	handleFunc("POST /post/{post_id}/vote/{option_index}", h.VoteOnPost)
 
 	// follow
-	mux.HandleFunc("POST /follow/{user_id}", h.FollowUser)
-	mux.HandleFunc("DELETE /follow/{user_id}", h.UnfollowUser)
+	handleFunc("POST /follow/{user_id}", h.FollowUser)
+	handleFunc("DELETE /follow/{user_id}", h.UnfollowUser)
 
-	mux.HandleFunc("POST /user/profile", h.UpdateProfile)
+	handleFunc("POST /user/profile", h.UpdateProfile)
 
 	// likes
-	mux.HandleFunc("POST /post/{id}/liked", h.AddPostLike)
-	mux.HandleFunc("DELETE /post/{id}/liked", h.RemovePostLike)
+	handleFunc("POST /post/{id}/liked", h.AddPostLike)
+	handleFunc("DELETE /post/{id}/liked", h.RemovePostLike)
 
 	// notifications
-	mux.HandleFunc("GET /notifications", h.GetAllNotificationByUserId)
-	mux.HandleFunc("GET /notifications/unread", h.GetUnreadNotificationsByUserId)
-	mux.HandleFunc("POST /notifications/markRead", h.MarkAllNotificationsAsRead)
-	mux.HandleFunc("POST /notifications/{id}/markRead", h.MarkNotificationAsReadById)
-	mux.HandleFunc("GET /notifications/hasUnread", h.HasUnreadNotifications)
-	mux.HandleFunc("GET /notifications/unreadCount", h.GetUnreadNotificationCount)
-	mux.HandleFunc("GET /notifications/read/time", h.GetReadNotificationsByUserIdWithTimeOffset)
-	mux.HandleFunc("GET /notifications/unread/time", h.GetUnreadNotificationsByUserIdWithTimeOffset)
+	handleFunc("GET /notifications", h.GetAllNotificationByUserId)
+	handleFunc("GET /notifications/unread", h.GetUnreadNotificationsByUserId)
+	handleFunc("POST /notifications/markRead", h.MarkAllNotificationsAsRead)
+	handleFunc("POST /notifications/{id}/markRead", h.MarkNotificationAsReadById)
+	handleFunc("GET /notifications/hasUnread", h.HasUnreadNotifications)
+	handleFunc("GET /notifications/unreadCount", h.GetUnreadNotificationCount)
+	handleFunc("GET /notifications/read/time", h.GetReadNotificationsByUserIdWithTimeOffset)
+	handleFunc("GET /notifications/unread/time", h.GetUnreadNotificationsByUserIdWithTimeOffset)
 
 	// comments
-	mux.HandleFunc("POST /post/{post_id}/comment", h.AddCommentToPostById)
-	mux.HandleFunc("POST /post/{post_id}/comment/{comment_id}/liked", h.AddCommentLike)
-	mux.HandleFunc("DELETE /post/{post_id}/comment/{comment_id}/liked", h.RemoveCommentLike)
+	handleFunc("POST /post/{post_id}/comment", h.AddCommentToPostById)
+	handleFunc("POST /post/{post_id}/comment/{comment_id}/liked", h.AddCommentLike)
+	handleFunc("DELETE /post/{post_id}/comment/{comment_id}/liked", h.RemoveCommentLike)
 
 	// blocking
-	mux.HandleFunc("POST /user/{user_id}/block", h.BlockUser)
-	mux.HandleFunc("DELETE /user/{user_id}/block", h.UnblockUser)
+	handleFunc("POST /user/{user_id}/block", h.BlockUser)
+	handleFunc("DELETE /user/{user_id}/block", h.UnblockUser)
 
-	mux.HandleFunc("GET /user/{id}", h.GetUserById)
-	mux.HandleFunc("GET /posts/following", h.GetPostsByFollowing)
-	mux.HandleFunc("GET /posts/all", h.GetAllPosts)
-	mux.HandleFunc("GET /posts/mutual", h.GetMutualFeed)
+	handleFunc("GET /user/{id}", h.GetUserById)
+	handleFunc("GET /posts/following", h.GetPostsByFollowing)
+	handleFunc("GET /posts/all", h.GetAllPosts)
+	handleFunc("GET /posts/mutual", h.GetMutualFeed)
 
-	mux.HandleFunc("GET /users/search", h.SearchUsers)
+	handleFunc("GET /users/search", h.SearchUsers)
 
 	// comments
-	mux.HandleFunc("GET /post/{id}/comments", h.GetCommentsByPost)
+	handleFunc("GET /post/{id}/comments", h.GetCommentsByPost)
 }
 
 func (h *Handler) GetIntPathParam(r *http.Request, paramName string) (int, error) {
