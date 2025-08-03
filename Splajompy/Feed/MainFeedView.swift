37f0a0b359b478843cc0c9a9d3ebd1eef93f28c1
Wesley Weisenberger <wesleynw@pm.me>
2025-08-02 22:21:52 -0400

    use .enumerated() instead

--- Splajompy/Feed/MainFeedView.swift
+++ Splajompy/Feed/MainFeedView.swift
@@ -114,8 +114,7 @@ struct MainFeedView: View {
   private var postList: some View {
     ScrollView {
       LazyVStack(spacing: 0) {
-        ForEach(viewModel.posts.indices, id: \.self) { index in
-          let post = viewModel.posts[index]
+        ForEach(Array(viewModel.posts.enumerated()), id: \.element.id) { index, post in
           PostView(
             post: post,
             postManager: postManager,
@@ -123,7 +122,6 @@ struct MainFeedView: View {
             onLikeButtonTapped: { viewModel.toggleLike(on: post) },
             onPostDeleted: { viewModel.deletePost(on: post) }
           )
-          .id(post.id)
           .geometryGroup()
           .onAppear {
             handlePostAppear(post: post, index: index)
