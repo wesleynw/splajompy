ee6d37eca2a1ca4000817dc44e1b1b53689c8d96
Wesley Weisenberger <wesleynw@pm.me>
2025-08-08 10:34:23 -0700

    fix: aspect ratio fill for notification photos, not fit (#215)

--- Splajompy/Notifications/NotificationImageView.swift
+++ Splajompy/Notifications/NotificationImageView.swift
@@ -3,38 +3,8 @@ import SwiftUI
 
 struct NotificationImageView: View {
   let url: String
-  let width: Int32
-  let height: Int32
 
   private static let targetSize: CGFloat = 40
-
-  private static var scale: CGFloat {
-    #if os(iOS)
-      return UIScreen.main.scale
-    #else
-      return NSScreen.main?.backingScaleFactor ?? 1.0
-    #endif
-  }
-
-  private static var targetPixelSize: CGFloat {
-    return targetSize * scale
-  }
-
-  private var aspectRatio: CGFloat {
-    CGFloat(width) / CGFloat(height)
-  }
-
-  private var processorSize: CGSize {
-    aspectRatio > 1
-      ? CGSize(
-        width: Self.targetPixelSize * aspectRatio,
-        height: Self.targetPixelSize
-      )
-      : CGSize(
-        width: Self.targetPixelSize,
-        height: Self.targetPixelSize / aspectRatio
-      )
-  }
 
   var body: some View {
     if let url = URL(string: url) {
@@ -42,17 +12,19 @@ struct NotificationImageView: View {
         state in
         if let image = state.image {
           image.resizable()
+            .aspectRatio(contentMode: .fill)
         } else {
           ProgressView()
             .frame(maxWidth: Self.targetSize, maxHeight: Self.targetSize)
         }
       }
       .processors([
-        .resize(size: processorSize),
-        .roundedCorners(radius: 20),
+        .resize(
+          size: CGSize(width: Self.targetSize, height: Self.targetSize), unit: .points,
+          contentMode: .aspectFill)
       ])
       .frame(width: Self.targetSize, height: Self.targetSize)
-      .aspectRatio(contentMode: .fill)
+      .clipShape(RoundedRectangle(cornerRadius: 8))
     }
   }
 }
