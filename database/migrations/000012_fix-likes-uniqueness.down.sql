335f60b6b63bf5c7ae77af3d465d5e01acea03cb
Wesley Weisenberger <wesleynw@pm.me>
2025-09-01 18:37:16 -0400

    format

--- database/migrations/000012_fix-likes-uniqueness.down.sql
+++ database/migrations/000012_fix-likes-uniqueness.down.sql
@@ -1,9 +1,5 @@
-BEGIN;
-
 DROP INDEX IF EXISTS likes_post_unique;
 DROP INDEX IF EXISTS likes_comment_unique;
 
 ALTER TABLE likes ADD CONSTRAINT likes_user_id_post_id_comment_id_is_post_unique
     UNIQUE (user_id, post_id, comment_id, is_post);
-
-COMMIT;
