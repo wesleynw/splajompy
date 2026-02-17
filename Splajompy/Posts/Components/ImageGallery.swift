03595455ba93326c0110af2338eb1680529edb6a
Wesley Weisenberger <wesleynw@pm.me>
2026-02-17 17:17:39 -0500

    lint

--- Splajompy/Posts/Components/ImageGallery.swift
+++ Splajompy/Posts/Components/ImageGallery.swift
@@ -186,8 +186,7 @@ struct ImageGallery: View {
     topTrailing: CGFloat = 6
   ) -> some View {
     Group {
-      if index < images.count, let url = URL(string: images[index].imageBlobUrl)
-      {
+      if index < images.count, let url = URL(string: images[index].imageBlobUrl) {
         LazyImage(url: url) {
           state in
           if let image = state.image {
