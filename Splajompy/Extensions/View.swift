a0a9881571d2220e9faf53ee0367b5716c7a9d23
Wesley Weisenberger <wesleynw@pm.me>
2026-04-08 19:32:11 -0400

    chore: only apply serifs sometimes (#568)

--- Splajompy/Extensions/View.swift
+++ Splajompy/Extensions/View.swift
@@ -17,10 +17,12 @@ extension View {
     self
       .navigationTitle(title)
       #if os(iOS)
+        .navigationBarTitleDisplayMode(.inline)
         .toolbar {
           ToolbarItem(placement: .principal) {
             Text(title)
             .font(.headline)
+            .fontDesign(.serif)
           }
         }
       #endif
