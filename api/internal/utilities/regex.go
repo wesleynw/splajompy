7b30e2d6b698f742f0d7305224901fabebda73c8
Wesley Weisenberger <wesleynw@pm.me>
2026-04-18 12:44:54 -0400

    fix

--- api/internal/utilities/regex.go
+++ api/internal/utilities/regex.go
@@ -4,7 +4,7 @@ import "regexp"
 
 var (
 	UsernamePattern = `[a-zA-Z0-9](?:[a-zA-Z0-9._]*[a-zA-Z0-9])?`
-	MentionRegex    = regexp.MustCompile(`(?:^| )@(` + UsernamePattern + `)`)
+	MentionRegex    = regexp.MustCompile(`(?:^|[\s])@(` + UsernamePattern + `)`)
 	UsernameRegex   = regexp.MustCompile(`^` + UsernamePattern + `$`)
 	EmailRegex      = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 )
