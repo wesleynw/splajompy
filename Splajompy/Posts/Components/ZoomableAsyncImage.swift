1d2394f6f326eeecfcaffe1f937eaf2f4091e9f1
Wesley Weisenberger <wesleynw@pm.me>
2026-04-22 19:25:36 -0400

    stuff

--- Splajompy/Posts/Components/ZoomableAsyncImage.swift
+++ Splajompy/Posts/Components/ZoomableAsyncImage.swift
@@ -1,112 +1,119 @@
 import NukeUI
 import SwiftUI
 
-#if os(iOS)
+/// A zoomable, pannable async image display. Wraps UIScrollView and LazyImageView.
+struct ZoomableAsyncImage: UIViewRepresentable {
+  let imageUrl: String
+  var cornerRadius: CGFloat = 0
+  var isShowingAccessories: Bool
 
-  /// A zoomable, pannable async image display. Wraps UIScrollView and LazyImageView.
-  struct ZoomableAsyncImage: UIViewRepresentable {
-    let imageUrl: String
-    var cornerRadius: CGFloat = 0
-    var isZooming: Binding<Bool> = .constant(false)
+  func makeUIView(context: Context) -> some UIView {
+    let scrollView = UIScrollView()
+    let imageLoaderView = LazyImageView()
 
-    func makeUIView(context: Context) -> some UIView {
-      let scrollView = UIScrollView()
-      let imageLoaderView = LazyImageView()
+    scrollView.delegate = context.coordinator
+    context.coordinator.imageLoaderView = imageLoaderView
 
-      scrollView.delegate = context.coordinator
-      context.coordinator.imageLoaderView = imageLoaderView
-      context.coordinator.isZooming = isZooming
+    scrollView.minimumZoomScale = 0.8
+    scrollView.maximumZoomScale = 4
 
-      scrollView.minimumZoomScale = 1
-      scrollView.maximumZoomScale = 4
+    let doubleTapRecognizer = UITapGestureRecognizer(
+      target: context.coordinator,
+      action: #selector(Coordinator.handleDoubleTap(_:))
+    )
+    doubleTapRecognizer.numberOfTapsRequired = 2
+    scrollView.addGestureRecognizer(doubleTapRecognizer)
 
-      let doubleTapRecognizer = UITapGestureRecognizer(
-        target: context.coordinator,
-        action: #selector(Coordinator.handleDoubleTap(_:))
-      )
-      doubleTapRecognizer.numberOfTapsRequired = 2
-      scrollView.addGestureRecognizer(doubleTapRecognizer)
+    scrollView.showsHorizontalScrollIndicator = false
+    scrollView.showsVerticalScrollIndicator = false
+    scrollView.contentInsetAdjustmentBehavior = .never
 
-      scrollView.backgroundColor = .clear
+    imageLoaderView.placeholderView = UIActivityIndicatorView()
+    imageLoaderView.processors = [
+      .resize(size: UIScreen.main.bounds.size, contentMode: .aspectFit),
+      .roundedCorners(radius: cornerRadius, unit: .points),
+    ]
+    imageLoaderView.url = URL(string: imageUrl)
+    imageLoaderView.imageView.contentMode = .scaleAspectFit
+    imageLoaderView.imageView.backgroundColor = .clear
 
-      imageLoaderView.placeholderView = UIActivityIndicatorView()
-      imageLoaderView.processors = [
-        .resize(size: UIScreen.main.bounds.size, contentMode: .aspectFit),
-        .roundedCorners(radius: cornerRadius, unit: .points),
-      ]
-      imageLoaderView.url = URL(string: imageUrl)
-      imageLoaderView.imageView.contentMode = .scaleAspectFit
-      imageLoaderView.imageView.backgroundColor = .clear
+    imageLoaderView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.addSubview(imageLoaderView)
 
-      imageLoaderView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-      scrollView.addSubview(imageLoaderView)
+    NSLayoutConstraint.activate([
+      imageLoaderView.leadingAnchor.constraint(
+        equalTo: scrollView.leadingAnchor
+      ),
+      imageLoaderView.trailingAnchor.constraint(
+        equalTo: scrollView.trailingAnchor
+      ),
+      imageLoaderView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      imageLoaderView.bottomAnchor.constraint(
+        equalTo: scrollView.bottomAnchor
+      ),
+      imageLoaderView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+      imageLoaderView.heightAnchor.constraint(
+        equalTo: scrollView.heightAnchor
+      ),
+    ])
 
-      return scrollView
-    }
+    scrollView.addSubview(imageLoaderView)
 
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-      guard context.coordinator.imageLoaderView?.url != URL(string: imageUrl) else { return }
-      context.coordinator.imageLoaderView?.url = URL(string: imageUrl)
-    }
+    return scrollView
+  }
 
-    func makeCoordinator() -> Coordinator {
-      Coordinator()
-    }
-
-    class Coordinator: NSObject, UIScrollViewDelegate {
-      var isZooming: Binding<Bool> = .constant(false)
-      weak var imageLoaderView: LazyImageView?
-
-      func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        imageLoaderView
-      }
-
-      func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        isZooming.wrappedValue = scrollView.zoomScale > 1
-      }
-
-      func scrollViewDidEndZooming(
-        _ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat
-      ) {
-        isZooming.wrappedValue = scale > 1
-      }
-
-      @objc func handleDoubleTap(_ gesture: UITapGestureRecognizer) {
-        guard let scrollView = gesture.view as? UIScrollView else { return }
-        if scrollView.zoomScale == 1 {
-          let point = gesture.location(in: imageLoaderView)
-          let zoomRect = zoomRectForScale(
-            scale: scrollView.maximumZoomScale,
-            center: point,
-            scrollView: scrollView
-          )
-          scrollView.zoom(to: zoomRect, animated: true)
-        } else {
-          scrollView.setZoomScale(1, animated: true)
-        }
-      }
-
-      private func zoomRectForScale(
-        scale: CGFloat,
-        center: CGPoint,
-        scrollView: UIScrollView
-      ) -> CGRect {
-        var zoomRect = CGRect.zero
-        zoomRect.size.height = scrollView.frame.size.height / scale
-        zoomRect.size.width = scrollView.frame.size.width / scale
-        zoomRect.origin.x = center.x - (zoomRect.size.width / 2.0)
-        zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0)
-        return zoomRect
-      }
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+    let newURL = URL(string: imageUrl)
+    if context.coordinator.imageLoaderView?.url != newURL {
+      context.coordinator.imageLoaderView?.url = newURL
     }
   }
 
-  #Preview {
-    let url: String =
-      "https://splajompy-bucket.nyc3.cdn.digitaloceanspaces.com/development/posts/1/9278fc8a-401b-4145-83bb-ef05d4d52632.jpeg"
-
-    ZoomableAsyncImage(imageUrl: url)
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
+  func makeCoordinator() -> Coordinator {
+    Coordinator()
   }
 
-#endif
+  class Coordinator: NSObject, UIScrollViewDelegate {
+    weak var imageLoaderView: LazyImageView?
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+      imageLoaderView
+    }
+
+    @objc func handleDoubleTap(_ gesture: UITapGestureRecognizer) {
+      guard let scrollView = gesture.view as? UIScrollView else { return }
+      if scrollView.zoomScale == 1 {
+        let point = gesture.location(in: imageLoaderView)
+        let zoomRect = zoomRectForScale(
+          scale: scrollView.maximumZoomScale,
+          center: point,
+          scrollView: scrollView
+        )
+        scrollView.zoom(to: zoomRect, animated: true)
+      } else {
+        scrollView.setZoomScale(1, animated: true)
+      }
+    }
+
+    private func zoomRectForScale(
+      scale: CGFloat,
+      center: CGPoint,
+      scrollView: UIScrollView
+    ) -> CGRect {
+      var zoomRect = CGRect.zero
+      zoomRect.size.height = scrollView.frame.size.height / scale
+      zoomRect.size.width = scrollView.frame.size.width / scale
+      zoomRect.origin.x = center.x - (zoomRect.size.width / 2.0)
+      zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0)
+      return zoomRect
+    }
+  }
+}
+
+#Preview {
+  let url: String =
+    "https://splajompy-bucket.nyc3.cdn.digitaloceanspaces.com/development/posts/1/9278fc8a-401b-4145-83bb-ef05d4d52632.jpeg"
+
+  ZoomableAsyncImage(imageUrl: url, isShowingAccessories: true)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+}
