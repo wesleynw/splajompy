6931151333c09f87ade5ab045d346cbb3f98036d
Wesley Weisenberger <wesleynw@pm.me>
2026-06-04 22:46:28 -0400

    feat: onboarding for push notifications (#673)

---
+++ Splajompy/Onboarding/OnboardingSheetViewModifier.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct OnboardingSheetViewModifier: ViewModifier {
+  @AppStorage("image_layout_preference") private var imageLayoutPreference: ImageLayoutPreference =
+    .undecided
+  @State private var tempImageLayoutPreference: ImageLayoutPreference =
+    .undecided
+
+  @AppStorage("hasCompletedPushNotificationOnboarding") private
+    var hasCompletedPushNotificationOnboarding: Bool = false
+
+  @State private var isNavigationToPushNotificationOnboarding: Bool = false
+
+  func body(content: Content) -> some View {
+    content
+      .sheet(
+        isPresented: .constant(
+          imageLayoutPreference == .undecided
+            || (!hasCompletedPushNotificationOnboarding)
+        )
+      ) {
+        Group {
+          NavigationStack {
+            if imageLayoutPreference == .undecided {
+              ImageLayoutOnboardingView(
+                onComplete: {
+                  if !hasCompletedPushNotificationOnboarding {
+                    isNavigationToPushNotificationOnboarding = true
+                  } else {
+                    imageLayoutPreference = tempImageLayoutPreference
+                  }
+                },
+                preference: $tempImageLayoutPreference
+              )
+              .navigationDestination(
+                isPresented: $isNavigationToPushNotificationOnboarding
+              ) {
+                PushNotificationsOnboardingView(
+                  onComplete: {
+                    imageLayoutPreference = tempImageLayoutPreference
+                    hasCompletedPushNotificationOnboarding = true
+                  }
+                )
+                .toolbar(.hidden, for: .navigationBar)
+              }
+            } else {
+              PushNotificationsOnboardingView(
+                onComplete: {
+                  hasCompletedPushNotificationOnboarding = true
+                }
+              )
+            }
+          }
+        }
+        .interactiveDismissDisabled()
+      }
+  }
+}
