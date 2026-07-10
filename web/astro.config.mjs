0a1fd5a07765d7f3a17ca488b4eb5056b4e4fe85
Wesley Weisenberger <wesleynw@pm.me>
2026-07-10 16:41:36 -0400

    update website with new design

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Splajompy",
+    cssVariable: "--font-splajompy",
+    options: {
+      variants: [{
+        src: ['./src/assets/Splajompy.ttf'],
+        weight: 'normal',
+        style: 'normal'
+      }]
+    }
+  }],
   site: "https://splajompy.com",
   vite: {
     plugins: [tailwindcss()],
