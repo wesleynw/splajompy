d68246f87b8ee0f26af636ce89fd978b1cd86324
Wesley Weisenberger <wesleynw@pm.me>
2026-08-22 14:48:06 -0700

    fix: go infra lint from deprecated property (#825)

--- infra/main.go
+++ infra/main.go
@@ -325,8 +325,12 @@ func main() {
 						Rule: pulumi.String("DOMAIN_FAILED"),
 					},
 				},
-				Domains: pulumi.StringArray{
-					pulumi.Sprintf("api.%s", domain.Name),
+				DomainNames: digitalocean.AppSpecDomainNameArray{
+					digitalocean.AppSpecDomainNameArgs{
+						Name: pulumi.Sprintf("api.%s", domain.Name),
+						Type: pulumi.String("PRIMARY"),
+						Zone: domain.Name,
+					},
 				},
 				Name:   pulumi.String("splajompy-app"),
 				Region: pulumi.String("nyc"),
