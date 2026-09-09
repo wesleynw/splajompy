39331d1d53f8de32a0eb4c7a1ff13c000b10de88
Wesley Weisenberger <wesleynw@pm.me>
2026-09-08 21:21:14 -0400

    feat: party invite (#866)

--- Splajompy/Onboarding/OnboardingSheetViewModifier.swift
+++ Splajompy/Onboarding/OnboardingSheetViewModifier.swift
@@ -4,10 +4,14 @@ import SwiftUI
 struct OnboardingSheetViewModifier: ViewModifier {
   @AppStorage("image_layout_preference") private var imageLayoutPreference: ImageLayoutPreference =
     .undecided
-  @State private var stagedImageLayoutPreference: ImageLayoutPreference = .undecided
+  @State private var stagedImageLayoutPreference: ImageLayoutPreference =
+    .undecided
 
   @AppStorage("hasCompletedPushNotificationOnboarding") private
     var hasCompletedPushNotificationOnboarding: Bool = false
+
+  @AppStorage("hasViewedSplajompartyInvite09252026") private
+    var hasViewedSplajompartyInvite: Bool = false
 
   @State private var isNavigationToPushNotificationOnboarding: Bool = false
 
@@ -21,8 +25,21 @@ struct OnboardingSheetViewModifier: ViewModifier {
       )
   }
 
+  var shouldShowSplajomparty: Bool {
+    !hasViewedSplajompartyInvite
+      && PostHogSDK.shared.isFeatureEnabled("splajomparty-invite-09252026")
+  }
+
   func body(content: Content) -> some View {
     content
+      .sheet(
+        isPresented: .constant(
+          shouldShowSplajomparty
+            && !(shouldShowImageOnboarding || shouldShowNotificationsOnboarding)
+        )
+      ) {
+        PartyInviteView(onDismiss: { hasViewedSplajompartyInvite = true })
+      }
       .sheet(
         isPresented: .constant(
           shouldShowImageOnboarding || shouldShowNotificationsOnboarding
