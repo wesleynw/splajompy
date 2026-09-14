a887912859b04983de889d36045d233355370b6e
Wesley Weisenberger <wesleynw@pm.me>
2026-09-14 17:52:24 -0400

    lint

--- Splajompy/Delegates/NSApplicationDelegate.swift
+++ Splajompy/Delegates/NSApplicationDelegate.swift
@@ -36,8 +36,10 @@ class AppDelegate: NSObject, NSApplicationDelegate,
       properties: ["error": error.localizedDescription]
     )
   }
-  
-  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication)
+    -> Bool
+  {
     return true
   }
 
